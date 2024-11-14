e1770db43f8071dbd3ee70d7c109be1b1ab00a64
Minicapinon <bava080502hgtrtna6@cbtis217.edu.mx>
2024-11-14 15:26:53 +0100

    Archivos vacios

---
+++ Proyecto_Transversal/MainForm.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by CapinonMStudio.
+ * User: CapinonM
+ * Date: 14/11/2024
+ * Time: 03:16 p. m.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Proyecto_Transversal
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		
+		}
+	}
+}
---
+++ Proyecto_Transversal/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: cc2-pc06
+ * Date: 14/11/2024
+ * Time: 03:16 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace Proyecto_Transversal
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
